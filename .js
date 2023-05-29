b939ad5fffbea50877d41125b4a1083402c4f7be
iammarcos77 <126510144+iammarcos77@users.noreply.github.com>
2023-05-29 17:40:00 -0600

    Create .js

---
+++ .js
@@ -0,0 +1,28 @@
+package com.example.marcosb
+
+fun main() {
+    var num1: Int = 5
+    var num2: Int = 10
+    var suma = num1 + num2
+    println("La suma es $suma")
+    var inicial: Char = '*'
+    println("El simbolo es $inicial")
+    var nombre: String = "Sheila Izara"
+    println("El Simbolo es $nombre")
+    var num4: Int = 4
+    var num5: Int = 20
+    var multiplicacion = num4 * num5
+    println("La multiplicacion es $multiplicacion")
+
+    var num6: Int = 40
+    var num7: Int = 5
+    var resta = num6 - num7
+    println("La resta es $resta")
+
+    val result: Math.pow (num1.toDouble(), num2.toDouble())
+    println("La num1 a la num2 $result")
+
+
+
+
+}
